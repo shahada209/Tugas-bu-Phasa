ed43925b14ba46788931877fcd614ffcd051a7b9
shahada209 <asyifa_shahada_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:45:43 +0700

    Add files via upload

---
+++ Guru.java
@@ -0,0 +1,21 @@
+//object Class
+public class Guru {
+    String Guru;
+    int id;
+    String nama;
+    String mapel;
+    String alamat;
+
+    /*Constructor*/
+    public Guru() {
+        id = 5;
+        nama = "";
+        mapel = "";
+        alamat = "";
+
+
+
+
+    }
+
+}
---
+++ TesGuru.java
@@ -0,0 +1,59 @@
+public class TesGuru {
+    public static void main(String[] args) {
+        //membuat objek
+        Guru buaulia = new Guru();
+        Guru pakdiaur = new Guru();
+        Guru bufeni = new  Guru();
+        Guru pakandre = new Guru();
+        Guru bupasha = new Guru();
+        buaulia.id = 1;
+        buaulia.nama = "buAulia";
+        buaulia.mapel = "produktifRpl";
+        buaulia.alamat = "malang";
+        pakdiaur.id = 2;
+        pakdiaur.nama = "pakDiaur";
+        pakdiaur.mapel = "pjok";
+        pakdiaur.alamat = "malang";
+        bufeni.id = 3;
+        bufeni.nama = "buFeni";
+        bufeni.mapel = "mtk";
+        bufeni.alamat = "malang";
+        pakandre.id = 4;
+        pakandre.nama = "pakAndre";
+        pakandre.mapel = "b inggris";
+        pakandre.alamat = "malang";
+        bupasha.id = 5;
+        bupasha.nama = "buPasha";
+        bupasha.mapel = "produktifRpl";
+        bupasha.alamat = "malang";
+        System.out.println("Ini data buaulia");
+        System.out.println(buaulia.id);
+        System.out.println(buaulia.nama);
+        System.out.println(buaulia.mapel);
+        System.out.println(buaulia.alamat);
+        System.out.println("Ini data pakdiaur");
+        System.out.println(pakdiaur.id);
+        System.out.println(pakdiaur.nama);
+        System.out.println(pakdiaur.mapel);
+        System.out.println(pakdiaur.alamat);
+        System.out.println("Ini data bufeni");
+        System.out.println(bufeni.id);
+        System.out.println(bufeni.nama);
+        System.out.println(bufeni.mapel);
+        System.out.println(bufeni.alamat);
+        System.out.println("Ini data pakandre");
+        System.out.println(pakandre.id);
+        System.out.println(pakandre.nama);
+        System.out.println(pakandre.mapel);
+        System.out.println(pakandre.alamat);
+        System.out.println("Ini data bupasha");
+        System.out.println(bupasha.id);
+        System.out.println(bupasha.nama);
+        System.out.println(bupasha.mapel);
+        System.out.println(bupasha.alamat);
+
+        
+
+
+    }
+} 
